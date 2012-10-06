a109568db3bd62c74003027e81d85615aaf74d7d
sqrabs <sqrabs@gmail.com>
2012-10-07 03:05:49 +0800

    更新插件版本

--- .emacs.d/init.d/yasnippet.el
+++ .emacs.d/init.d/yasnippet.el
@@ -2,6 +2,6 @@
 (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet)     
 (setq yas/trigger-key (kbd "C-c <kp-multiply>"))
-(yas/initialize)
+(yas-global-mode 1)
 (yas/load-directory "~/.emacs.d/plugins/yasnippet/snippets")
 (yas/load-directory "~/.emacs.d/plugins/django-mode/snippets")
